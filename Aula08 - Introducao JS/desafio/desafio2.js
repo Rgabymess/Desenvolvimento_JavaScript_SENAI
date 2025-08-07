c967eda99d460e4d4768f3e8d32ffd4e702432c6
Gabriel Messora <gabriel.messora@gmail.com>
2025-08-07 17:06:50 -0300

    .

--- Aula08 - Introducao JS/desafio/desafio2.js
+++ Aula08 - Introducao JS/desafio/desafio2.js
@@ -9,3 +9,4 @@ raiz = Math.sqrt(numero)
 console.log("O dobro do " + numero + "é =" +dobro)
 console.log("O triplo do " + numero + "é =" +triplo)
 console.log("O dobro do " + numero + "é =" +raiz)
+
